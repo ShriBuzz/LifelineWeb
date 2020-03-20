df4393ff07d58698df817a7113262dd666eb4df4
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2020-03-20 13:41:17 +0545

    Restructure components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from "react";
 
 import NavBar from "./components/NavBar";
 import Signup from "./components/Signup";
-import Driver from "./screens/Driver";
 
 const App = () => {
   return (
     <React.Fragment>
       <NavBar />
       <Signup />
-      <Driver />
     </React.Fragment>
   );
 };
