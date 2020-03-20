095c451340334189f9e5480ce6c6edaca0f381bc
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2020-03-20 13:40:19 +0545

    Add initial states for input

--- src/screens/Driver/index.js
+++ src/screens/Driver/index.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Container from "@material-ui/core/Container";
 import Box from "@material-ui/core/Box";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
 
+import Buttons from "../../components/Button";
+
 import * as D from "./styles";
 
 const Driver = () => {
+  const [name, setName] = useState("");
+  const [contact, setContact] = useState();
+  const [email, setEmail] = useState("");
+  const [driver_id, setDriverId] = useState("");
+  const [password, setPassword] = useState("");
+  const [upload, setUpload] = useState();
+  const [loading, setLoading] = useState(false);
+
+  function fetchData() {
+    setLoading(true);
+    //API call here
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }
+
   return (
     <Container fluid style={D.bg}>
       <Typography variant="h5" style={D.text}>
@@ -19,6 +37,7 @@ const Driver = () => {
           id="standard"
           label="Name"
           type="text"
+          value={name}
           placeholder="Enter your name"
           autoComplete="text"
           style={D.input}
@@ -28,6 +47,7 @@ const Driver = () => {
           id="standard"
           label="Email"
           type="email"
+          value={email}
           placeholder="Enter your email"
           autoComplete="email"
           style={D.input}
@@ -37,6 +57,7 @@ const Driver = () => {
           id="standard"
           label="Contact"
           type="number"
+          value={contact}
           placeholder="Enter your mobile no."
           autoComplete="number"
           style={D.input}
@@ -46,6 +67,7 @@ const Driver = () => {
           id="standard"
           label="Driver Id"
           type="text"
+          value={driver_id}
           placeholder="Enter your driver id"
           autoComplete="text"
           style={D.input}
@@ -55,10 +77,12 @@ const Driver = () => {
           id="standard"
           label="Password"
           type="password"
+          value={password}
           placeholder="Enter your password"
           autoComplete="current-password"
           style={D.input}
         />
+        <Buttons title="Submit" loading={loading} onSubmit={fetchData} />
       </Box>
     </Container>
   );
