08a1c393f62523e0e96509d98a8cc0fb017f5adf
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2020-03-21 12:54:25 +0545

    Add Home screen

---
+++ src/screens/Home/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import NavBar from "../../components/NavBar";
+import Signup from "../../components/Signup";
+
+const Home = () => {
+  return (
+    <React.Fragment>
+      <NavBar />
+      <Signup />
+    </React.Fragment>
+  );
+};
+
+export default Home;
