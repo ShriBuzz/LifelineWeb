285443870f8f9d928391c9909cd07b8d6c753628
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2020-03-21 21:23:36 +0545

    Add driver list component

---
+++ src/components/Drawer/styles.js
@@ -0,0 +1,8 @@
+export const title = {
+  display: "flex",
+  justifyContent: "center",
+  fontWeight: "600",
+  fontSize: "1.2em",
+  marginTop: "0.5em",
+  marginBottom: "0.5em"
+};
---
+++ src/screens/Driver List/index.js
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+
+import {
+  Box,
+  Typography,
+  Table,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TableBody
+} from "@material-ui/core";
+import axios from "axios";
+
+import NavBar from "../../components/NavBar";
+import Buttons from "../../components/Button";
+
+import * as D from "./styles";
+
+const DriverList = () => {
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [email, setEmail] = useState([]);
+  const [contact, setContact] = useState([]);
+  const [driverId, setDriverId] = useState([]);
+
+  function getUsers(e) {
+    e.preventDefault();
+    setLoading(true);
+    const nlist = [];
+    const elist = [];
+    const clist = [];
+    const dlist = [];
+    axios.get("/driver").then(res => {
+      res.data.map(
+        c => (
+          nlist.push(c.name),
+          elist.push(c.email),
+          clist.push(c.contact),
+          dlist.push(c.driver_id)
+        )
+      );
+      setUsers(nlist);
+      setEmail(elist);
+      setContact(clist);
+      setDriverId(dlist);
+    });
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }
+
+  return (
+    <React.Fragment>
+      <NavBar />
+      <Box component="div" style={D.container}>
+        <Buttons title={"View"} loading={loading} onSubmit={getUsers} />
+        <TableContainer style={D.tableContainer}>
+          <Table aria-label="simple table">
+            <TableBody>
+              <TableRow style={D.tableHeader}>
+                <TableCell align="right" style={D.cell}>
+                  Name
+                  {users.map(c => {
+                    return (
+                      <TableRow component="th" scope="row" style={D.row}>
+                        {c}{" "}
+                      </TableRow>
+                    );
+                  })}
+                </TableCell>
+                <TableCell align="right" style={D.cell}>
+                  Email
+                  {email.map(c => {
+                    return (
+                      <TableRow component="th" scope="row" style={D.row}>
+                        {c}{" "}
+                      </TableRow>
+                    );
+                  })}
+                </TableCell>
+                <TableCell align="right" style={D.cell}>
+                  Contact
+                  {contact.map(c => {
+                    return (
+                      <TableRow component="th" scope="row" style={D.row}>
+                        {c}{" "}
+                      </TableRow>
+                    );
+                  })}
+                </TableCell>
+                <TableCell align="right" style={D.cell}>
+                  Driver ID
+                  {driverId.map(c => {
+                    return (
+                      <TableRow component="th" scope="row" style={D.row}>
+                        {c}{" "}
+                      </TableRow>
+                    );
+                  })}
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </React.Fragment>
+  );
+};
+
+export default DriverList;
