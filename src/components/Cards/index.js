7a3c4fd0992db338c88d7d0ff20f7c04d61b9bc2
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2020-05-01 10:41:42 +0545

    Optimize component structure

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -8,7 +8,7 @@ import {
   CardContent,
   Avatar,
   Button,
-  Typography
+  Typography,
 } from "@material-ui/core";
 
 import Dummy from "../../assets/Profile.jpg";
@@ -27,17 +27,16 @@ const Cards = ({ users, type }) => {
     setOpen(false);
   };
 
-  const handleDelete = url => {
+  const handleDelete = (url) => {
     axios
       .delete(url)
-      .then(res => {
+      .then((res) => {
         console.log(res);
-        window.location.reload(false);
       })
-      .catch(error => console.log(error));
+      .catch((error) => console.log(error));
   };
 
-  const renderAvatar = key => {
+  const renderAvatar = (key) => {
     if (key === null) {
       return <Avatar style={{ width: 90, height: 90 }} src={Dummy} />;
     } else {
@@ -52,160 +51,92 @@ const Cards = ({ users, type }) => {
   };
 
   if (users == null) {
-    if (type === "driver") {
-      return (
-        <Card style={C.Container} key={"name"}>
-          <CardActionArea style={C.CardContainer}>
-            <Avatar style={{ width: 90, height: 90 }} src={Dummy} />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                Name: -----
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                Contact: -----
-                <br />
-                Email: -----
-                <br />
-                Driver ID: -----
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Button
-              size="small"
-              color="secondary"
-              onClick={() => setOpen(true)}
-            >
-              Edit
-            </Button>
-            <Button size="small" color="secondary">
-              Delete
-            </Button>
-          </CardActions>
-        </Card>
-      );
-    } else {
-      return (
-        <Card style={C.Container} key={"name"}>
-          <CardActionArea style={C.CardContainer}>
-            <Avatar style={{ width: 90, height: 90 }} src={Dummy} />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                Name: -----
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                Contact: -----
-                <br />
-                Email: -----
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Button size="small" color="secondary">
-              Edit
-            </Button>
-            <Button size="small" color="secondary">
-              Delete
-            </Button>
-          </CardActions>
-        </Card>
-      );
-    }
+    return (
+      <Card style={C.Container} key={"name"}>
+        <CardActionArea style={C.CardContainer}>
+          <Avatar style={{ width: 90, height: 90 }} src={Dummy} />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Name: -----
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              Contact: -----
+              <br />
+              Email: -----
+              <br />
+              {type === "driver" ? (
+                <Typography>Driver ID: -----</Typography>
+              ) : null}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="secondary">
+            Edit
+          </Button>
+          <Button size="small" color="secondary">
+            Delete
+          </Button>
+        </CardActions>
+      </Card>
+    );
   } else {
-    if (type === "driver") {
-      return users.map(data => (
-        <Card style={C.Container} key={data.contact}>
-          <CardActionArea style={C.CardContainer}>
-            {renderAvatar(data.contact)}
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                {data.name}
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                Contact: {data.contact}
-                <br />
-                Email: {data.email}
-                <br />
-                Driver ID: {data.driver_id}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Button
-              size="small"
-              color="secondary"
-              onClick={() => {
-                setKey(data.contact.toString());
-                setName(data.name.toString());
-                setEmail(data.email.toString());
-                setDriverId(data.driver_id.toString());
-                setOpen(true);
-              }}
-            >
-              Edit
-            </Button>
-            <Button
-              size="small"
-              color="secondary"
-              onClick={() => {
-                const url = "/driver/" + data.contact;
-                handleDelete(url);
-              }}
-            >
-              Delete
-            </Button>
-          </CardActions>
-          <Edit
-            title="Edit Driver info"
-            open={open}
-            handleClose={handleClose}
-            users={users}
-            o_contact={key}
-            o_name={r_name}
-            o_email={r_email}
-            o_driverid={r_driverid}
-            // o_img={r_img}
-          />
-        </Card>
-      ));
-    } else {
-      return users.map(data => (
-        <Card style={C.Container} key={data.contact}>
-          <CardActionArea style={C.CardContainer}>
-            {renderAvatar(data.contact)}
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                {data.name}
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                Contact: {data.contact}
-                <br />
-                Email: {data.email}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Button
-              size="small"
-              color="secondary"
-              onClick={() => console.log(data.contact)}
-            >
-              Edit
-            </Button>
-            <Button
-              size="small"
-              color="secondary"
-              onClick={() => {
-                const url = "/traffic/" + data.contact;
-                handleDelete(url);
-              }}
-            >
-              Delete
-            </Button>
-          </CardActions>
-        </Card>
-      ));
-    }
+    return users.map((data) => (
+      <Card style={C.Container} key={data.contact}>
+        <CardActionArea style={C.CardContainer}>
+          {renderAvatar(data.contact)}
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {data.name}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              Contact: {data.contact}
+              <br />
+              Email: {data.email}
+              <br />
+              {data.driver_id ? (
+                <Typography>Driver ID: {data.driver_id}</Typography>
+              ) : null}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button
+            size="small"
+            color="secondary"
+            onClick={() => {
+              setKey(data.contact.toString());
+              setName(data.name.toString());
+              setEmail(data.email.toString());
+              setDriverId(data.driver_id.toString());
+              setOpen(true);
+            }}
+          >
+            Edit
+          </Button>
+          <Button
+            size="small"
+            color="secondary"
+            onClick={() => {
+              const url = "/driver/" + data.contact;
+              handleDelete(url);
+            }}
+          >
+            Delete
+          </Button>
+        </CardActions>
+        <Edit
+          title="Edit Driver info"
+          open={open}
+          handleClose={handleClose}
+          users={users}
+          o_contact={key}
+          o_name={r_name}
+          o_email={r_email}
+          o_driverid={r_driverid}
+        />
+      </Card>
+    ));
   }
 };
 
