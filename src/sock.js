73be62382c18c44b8847d02365c83eb4f5f2d1a4
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2020-05-13 15:34:31 +0545

    test socket code

---
+++ src/sock.js
@@ -0,0 +1,7 @@
+import openSocket from "socket.io-client";
+// const socket = openSocket("/");
+
+export function connect(x) {
+  // socket.emit("gps", 100);
+  // socket.on("details", (res) => x(res));
+}
