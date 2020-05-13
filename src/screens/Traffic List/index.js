43a2e341ef7a627397da66a24b57b2f4b89ea593
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2020-05-13 15:33:09 +0545

    simplify traffic list screen

--- src/screens/Traffic List/index.js
+++ src/screens/Traffic List/index.js
@@ -1,66 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
-import useTrafficData from "../../hooks/useTrafficData";
-
-import {
-  Box,
-  Table,
-  TableCell,
-  TableHead,
-  TableContainer,
-  TableRow,
-  TableBody,
-  CircularProgress,
-} from "@material-ui/core";
-import Paper from "@material-ui/core/Paper";
+import { Box, FormControlLabel, Switch } from "@material-ui/core";
 
 import NavBar from "../../components/NavBar";
-import GetTable from "../../components/GetTable";
+import TableView from "../../components/TableView";
 import Cards from "../../components/Cards";
-
 import * as T from "./styles";
 
 const TrafficList = () => {
-  const { users, loading } = useTrafficData();
-
-  if (!users || loading) {
-    return (
-      <>
-        <NavBar />
-        <Box component="div" style={T.container}>
-          <CircularProgress color="secondary" />
-        </Box>
-      </>
-    );
-  }
-
+  const [view, setView] = useState(false);
   return (
-    <React.Fragment>
+    <>
       <NavBar />
+
       <Box component="div" style={T.container}>
-        <TableContainer component={Paper} style={T.table}>
-          <Table aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell style={T.head}>Name</TableCell>
-                <TableCell align="right" style={T.head}>
-                  Contact
-                </TableCell>
-                <TableCell align="right" style={T.head}>
-                  Email
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <GetTable users={users} />
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <Box component="div" style={{ width: "100%", textAlign: "center" }}>
-          <Cards users={users} />
-        </Box>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={view}
+              onChange={() => setView(!view)}
+              name={view ? "Cards" : "Table"}
+            />
+          }
+          label={view ? "Cards" : "Table"}
+        />
+        {view ? (
+          <Box component="div" style={T.cardsContainer}>
+            <Cards />
+          </Box>
+        ) : (
+          <TableView />
+        )}
       </Box>
-    </React.Fragment>
+    </>
   );
 };
 
