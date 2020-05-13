bd2f20630a174e6b84cbb85921cfee713ac0d01c
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2020-05-13 13:46:33 +0545

    seperate tableview component

---
+++ src/components/TableView/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  TableContainer,
+  Paper,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+} from "@material-ui/core";
+
+import GetTable from "../GetTable";
+
+import * as D from "./styles";
+
+const TableView = ({ type }) => {
+  return (
+    <TableContainer component={Paper} style={D.table}>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell style={D.head}>Name</TableCell>
+            <TableCell align="right" style={D.head}>
+              Contact
+            </TableCell>
+            <TableCell align="right" style={D.head}>
+              Email
+            </TableCell>
+            <TableCell align="right" style={D.head}>
+              Driver ID
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <GetTable type={type} />
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default React.memo(TableView);
---
+++ src/components/TableView/styles.js
@@ -0,0 +1,19 @@
+export const table = {
+  marginTop: "1.2em",
+};
+
+export const head = {
+  fontWeight: 600,
+  textAlign: "center",
+};
+
+export const cell = {
+  textAlign: "center",
+};
+
+export const error = {
+  marginTop: 20,
+  textAlign: "center",
+  fontSize: 28,
+  color: "red",
+};
