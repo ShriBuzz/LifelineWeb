47830888bdecf7a67112bfab4bb7899296e5d782
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2020-05-14 13:53:27 +0545

    change env var

--- src/components/Edit/index.js
+++ src/components/Edit/index.js
@@ -7,7 +7,7 @@ import {
   DialogTitle,
   DialogActions,
   DialogContent,
-  Button
+  Button,
 } from "@material-ui/core";
 
 import * as E from "./styles";
@@ -21,13 +21,13 @@ const Edit = ({
   o_contact,
   o_name,
   o_email,
-  o_driverid
+  o_driverid,
 }) => {
   const [name, setName] = useState(o_name);
   const [contact, setContact] = useState(o_contact);
   const [email, setEmail] = useState(o_email);
   const [driver_id, setDriverId] = useState(o_driverid);
-  const url = "http://192.168.0.117:5000/get_driver_pic/" + o_contact;
+  const url = "https://lifeline-rats.herokuapp.com/get_driver_pic/" + o_contact;
 
   function handleUpdate(e) {
     e.preventDefault();
@@ -40,16 +40,16 @@ const Edit = ({
           name,
           email,
           driver_id,
-          contact
+          contact,
         },
         {}
       )
-      .then(response => {
+      .then((response) => {
         console.log(response.data);
         alert("succesfully updated!");
         window.location.reload(false);
       })
-      .catch(error => {
+      .catch((error) => {
         alert("failed to update!");
         console.log(error);
       });
@@ -72,35 +72,35 @@ const Edit = ({
               label={o_name}
               type="text"
               value={name}
-              onChange={e => setName(e.target.value)}
+              onChange={(e) => setName(e.target.value)}
               style={E.Input}
             />
             <TextField
               label={o_contact}
               type="text"
               value={contact}
-              onChange={e => setContact(e.target.value)}
+              onChange={(e) => setContact(e.target.value)}
               style={E.Input}
             />
             <TextField
               label={o_email}
               type="text"
               value={email}
-              onChange={e => setEmail(e.target.value)}
+              onChange={(e) => setEmail(e.target.value)}
               style={E.Input}
             />
             <TextField
               label={o_driverid}
               type="text"
               value={driver_id}
-              onChange={e => setDriverId(e.target.value)}
+              onChange={(e) => setDriverId(e.target.value)}
               style={E.Input}
             />
           </Box>
         </Box>
       </DialogContent>
       <DialogActions>
-        <Button onClick={e => handleUpdate(e)} color="secondary">
+        <Button onClick={(e) => handleUpdate(e)} color="secondary">
           Save
         </Button>
         <Button onClick={handleClose} color="secondary">
