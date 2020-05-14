2f3f34504391085e767d80341cad84eb35b573b2
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2020-05-14 13:42:26 +0545

    init socket

--- src/sock.js
+++ src/sock.js
@@ -1,7 +1,19 @@
-import openSocket from "socket.io-client";
-// const socket = openSocket("/");
-
-export function connect(x) {
-  // socket.emit("gps", 100);
-  // socket.on("details", (res) => x(res));
+import io from "socket.io-client";
+const socket = io("/");
+const data = {
+  name: "jsnf",
+  age: "12",
+  gender: "male",
+};
+const name = "harry";
+export function connect() {
+  socket.on("connect", function () {
+    socket.emit("json", data);
+  });
+  // socket.on("message", function (msg) {
+  //   console.log("Received message: ", msg);
+  // });
+  socket.on("json", function (json) {
+    console.log(json);
+  });
 }
