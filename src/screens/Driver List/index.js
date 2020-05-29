e7ac2430a81bc865f1e2639da5f748ee22cad35a
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2020-05-29 17:07:52 +0545

    Reposition edit component

--- src/screens/Driver List/index.js
+++ src/screens/Driver List/index.js
@@ -12,13 +12,21 @@ import useDriverData from "../../hooks/useDriverData";
 import NavBar from "../../components/NavBar";
 import TableView from "../../components/TableView";
 import Cards from "../../components/Cards";
+import Edit from "../../components/Edit";
 
 // styles
 import * as D from "./styles";
 
 const DriverList = () => {
   const [view, setView] = useState(true);
+  const [open, setOpen] = useState(false);
+  const [key, setKey] = useState();
+
   useDriverData();
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <>
@@ -37,12 +45,23 @@ const DriverList = () => {
         />
         {view ? (
           <Box component="div" style={D.cardsContainer}>
-            <Cards type="driver" />
+            <Cards type="driver" setOpen={setOpen} setKey={setKey} />
           </Box>
         ) : (
           <TableView type="driver" />
         )}
       </Box>
+      {key ? (
+        <Edit
+          title={"Edit Driver info"}
+          type="driver"
+          open={open}
+          setOpen={setOpen}
+          handleClose={handleClose}
+          o_contact={key}
+        />
+      ) : null}
+
       <ToastContainer />
     </>
   );
--- src/screens/Traffic List/index.js
+++ src/screens/Traffic List/index.js
@@ -12,13 +12,21 @@ import useTrafficData from "../../hooks/useTrafficData";
 import NavBar from "../../components/NavBar";
 import TableView from "../../components/TableView";
 import Cards from "../../components/Cards";
+import Edit from "../../components/Edit";
 
 // styles
 import * as T from "./styles";
 
 const TrafficList = () => {
   const [view, setView] = useState(true);
+  const [open, setOpen] = useState(false);
+  const [key, setKey] = useState();
+
   useTrafficData();
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <>
@@ -37,12 +45,22 @@ const TrafficList = () => {
         />
         {view ? (
           <Box component="div" style={T.cardsContainer}>
-            <Cards />
+            <Cards type="traffic" setOpen={setOpen} setKey={setKey} />
           </Box>
         ) : (
           <TableView />
         )}
       </Box>
+      {key ? (
+        <Edit
+          title={"Edit Traffic info"}
+          type="traffic"
+          open={open}
+          setOpen={setOpen}
+          handleClose={handleClose}
+          o_contact={key}
+        />
+      ) : null}
       <ToastContainer />
     </>
   );
