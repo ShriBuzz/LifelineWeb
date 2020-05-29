04d7225c5206b50444c4504e2f8e9bd95159770f
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2020-05-29 17:08:18 +0545

    dummy socket code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from "react";
 
 import Routes from "./navigation/router";
 
+// import { connect } from "./sock";
+
 const App = () => {
+  // connect();
   return <Routes />;
 };
 
--- src/sock.js
+++ src/sock.js
@@ -1,19 +1,40 @@
 import io from "socket.io-client";
 const socket = io("/");
-const data = {
-  name: "jsnf",
-  age: "12",
-  gender: "male",
+const obstruction = {
+  latitude: 20.425,
+  longitude: 84.234,
+  cause: "something happened",
+};
+const driverRoute = {
+  start: {
+    latitude: 20.425,
+    longitude: 84.234,
+  },
+  end: {
+    latitude: 20.425,
+    longitude: 84.234,
+  },
+};
+const driverGps = {
+  latitude: 20.425,
+  longitude: 84.234,
 };
 
 export function connect() {
   socket.on("connect", function () {
-    socket.emit("json", data);
+    socket.emit("obstruction", obstruction);
+    socket.emit("driver_route", driverRoute);
+    socket.emit("driver_gps", driverGps);
+
+    // socket.emit("message", "string");
   });
-  // socket.on("message", function (msg) {
-  //   console.log("Received message: ", msg);
+  // socket.on("message", function (lol) {
+  //   console.log("Received message: ", lol);
+  // });
+  // socket.on("json", function (json) {
+  //   console.log("data: ", json);
   // });
   socket.on("json", function (json) {
-    console.log(json);
+    console.log("data: ", json);
   });
 }
