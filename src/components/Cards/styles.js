f11989acfb590d301061c03ee14c20a3e69a51a4
Shrijan Bajracharya <shrijan.buzz.5@gmail.com>
2021-02-14 10:21:32 +0545

    change styles

--- src/components/Cards/styles.js
+++ src/components/Cards/styles.js
@@ -1,11 +1,12 @@
 export const Container = {
-  display: "inline-block",
-  maxWidth: 345,
-  margin: 15
+  display: 'inline-block',
+  // maxWidth: 345,
+  width: 'clamp(22vw, 345px, 100vw)',
+  margin: '15px 12px',
 };
 
 export const CardContainer = {
-  display: "flex",
-  flexDirection: "row",
-  padding: "10px 20px"
+  display: 'flex',
+  flexDirection: 'row',
+  padding: '10px 20px',
 };
--- src/screens/Driver List/styles.js
+++ src/screens/Driver List/styles.js
@@ -9,5 +9,37 @@ export const container = {
 
 export const cardsContainer = {
   width: '100%',
-  textAlign: 'center',
+  // textAlign: 'center',
+  display: 'flex',
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  justifyContent: 'center',
+};
+
+export const row = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  flexDirection: 'row',
+  width: '100%',
+};
+
+export const toggle = {
+  marginLeft: '50%',
+  transform: 'translateX(-38px)',
+};
+
+export const search = {
+  position: 'relative',
+  borderRadius: 5,
+  marginLeft: 10,
+  display: 'flex',
+  justifyContent: 'center',
+  backgroundColor: 'rgba(245,61,61,0.2)',
+};
+
+export const searchIcon = {
+  color: 'grey',
+  padding: '5px',
+  display: 'flex',
 };
